23bb3a627f7b8770df88f0fb9679bba2a3b541e5
Coopfox <cfoxall@uvic.ca>
2026-03-19 15:52:21 -0700

    Add files via upload

---
+++ data/MP10Poly_4.js
@@ -0,0 +1,1 @@
+var json_MP10Poly_4 = {"type":"FeatureCollection","name":"MP10Poly_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/SharUulLine_1.js
@@ -0,0 +1,1 @@
+var json_SharUulLine_1 = {"type":"FeatureCollection","name":"SharUulLine_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
